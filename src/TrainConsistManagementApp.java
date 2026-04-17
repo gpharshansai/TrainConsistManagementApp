90178459e3d17117b5bb8f582153774ddaa12877
gpharshansai <gpharshansai06@gmail.com>
2026-04-17 12:02:50 +0530

    UC2

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,11 +1,49 @@
+import java.util.ArrayList;
+
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        System.out.println("=== Train Consist Management App ===");
 
-        String[] bogies = new String[10];
-        int count = 0;
+        System.out.println("=================================");
+        System.out.println("UC2 - Add Passenger Bogies to Train");
+        System.out.println("=================================\n");
 
-        System.out.println("Train consist initialized.");
-        System.out.println("Initial bogie count: " + count);
+        ArrayList<String> bogies = new ArrayList<>();
+
+        bogies.add("Sleeper");
+        bogies.add("AC Chair");
+        bogies.add("First Class");
+
+        System.out.println("After Adding Bogies:");
+        System.out.print("Passenger Bogies : [");
+        for (int i = 0; i < bogies.size(); i++) {
+            System.out.print(bogies.get(i));
+            if (i < bogies.size() - 1) System.out.print(", ");
+        }
+        System.out.println("]\n");
+
+        bogies.remove("AC Chair");
+
+        System.out.println("After Removing 'AC Chair':");
+        System.out.print("Passenger Bogies : [");
+        for (int i = 0; i < bogies.size(); i++) {
+            System.out.print(bogies.get(i));
+            if (i < bogies.size() - 1) System.out.print(", ");
+        }
+        System.out.println("]\n");
+
+        boolean found = bogies.contains("Sleeper");
+
+        System.out.println("Checking if 'Sleeper' exists:");
+        System.out.println("Contains Sleeper? : " + found + "\n");
+
+        System.out.println("Final Train Passenger Consist:");
+        System.out.print("[");
+        for (int i = 0; i < bogies.size(); i++) {
+            System.out.print(bogies.get(i));
+            if (i < bogies.size() - 1) System.out.print(", ");
+        }
+        System.out.println("]\n");
+
+        System.out.println("UC2 operations completed successfully...");
     }
 }
